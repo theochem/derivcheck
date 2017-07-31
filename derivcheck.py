65566d5a0dc3314cd7b6525df36ecb0f5331293d
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2017-07-31 06:45:29 +0200

    Bump

--- derivcheck.py
+++ derivcheck.py
@@ -28,7 +28,7 @@ import numpy as np
 
 __all__ = ['diff_ridders', 'assert_deriv']
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 
 def diff_ridders(function, origin, stepsize, con=1.4, safe=2.0, maxiter=15):
